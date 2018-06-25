54592ae5facf5388701b95b65b1679ddbca03e73
Javier <sonico@ciencias.unam.mx>
2018-06-25 02:37:56 -0500

    Primer commit

---
+++ config/yargs.js
@@ -0,0 +1,23 @@
+const descripcion = {
+	alias: 'd',
+	demand: true
+};
+
+const completado = {
+	alias: 'c',
+	default: true
+};
+
+const argv = require('yargs')
+.command('crear', 'Crear un elemento por hacer', {descripcion})
+.command('actualizar', 'Actualiza el estado completado de una tarea', {
+	descripcion,
+	completado
+})
+.command('borrar', 'Borra el elemento por hacer', {descripcion})
+.help()
+.argv;
+
+module.exports = {
+	argv
+}
